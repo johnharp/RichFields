cfee299ad793d2c28ffd63619993926baa58447f
John Harper <jharper@waterstreetconsulting.com>
2020-09-10 22:05:39 -0500

    richfields

--- RichFields-Demo/App_Start/BundleConfig.cs
+++ RichFields-Demo/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace RichFields_Demo
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,6 +25,8 @@ namespace RichFields_Demo
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/jquery-ui.css",
+                      "~/Content/RichFields.css",
                       "~/Content/site.css"));
 
             // ----------------------------------------------------------
