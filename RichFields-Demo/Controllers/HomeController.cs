01a3ca62cc3a3397e116744187c10bbe81c1dfcb
John Harper <jharper@waterstreetconsulting.com>
2020-09-11 08:45:17 -0500

    Create a Save action so the example form can post back

--- RichFields-Demo/Controllers/HomeController.cs
+++ RichFields-Demo/Controllers/HomeController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RichFields_Demo.Models;
 
 namespace RichFields_Demo.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            
-            return View();
+            var model = new RichFieldsDemoViewModel();
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Index")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save()
+        {
+            var model = new RichFieldsDemoViewModel();
+            if (TryUpdateModel(model, "", new string[] {
+                "ExamplePercent", "ExampleMoney", "ExampleInt",
+                "ExampleDate", "ExampleMonthYear", "ExampleText" }))
+            {
+
+            }
+            return View(model);
         }
     }
 }
